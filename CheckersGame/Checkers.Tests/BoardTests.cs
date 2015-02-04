31b1a934f72186b238fc245468982806a70a11d0
Amadeus Wieczorek <git@amadeusw.com>
2015-02-03 21:51:07 -0800

    Adds unit test

---
+++ CheckersGame/Checkers.Tests/BoardTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Checkers.Core;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Checkers.Tests
+{
+    [TestClass]
+    public class BoardTests
+    {
+        [TestMethod]
+        public void TestBoardSetup()
+        {
+            var board = GameRules.startGame;
+            Assert.AreEqual(board.Where(x => x.IsBlackMan).Count(), 12);
+            Assert.AreEqual(board.Where(x => x.IsWhiteMan).Count(), 12);
+            Assert.AreEqual(board.Where(x => x.IsBlackKing).Count(), 0);
+            Assert.AreEqual(board.Where(x => x.IsBlackKing).Count(), 0);
+        }
+    }
+}
